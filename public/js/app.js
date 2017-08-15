0e677381a8a9300bb9587cd5699ab3bbded14ce4
Duong Pham <bovame94@gmail.com>
2017-08-15 22:00:54 +0700

    add front end

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,15 @@ socket.on('connect', function() {
 socket.on('message', function(message){
   console.log('New message:' + message.text);
 });
+
+$(document).ready(function(){
+  $("#message-form").on("submit", function(event){
+    event.preventDefault();
+
+    var $message = $(this).find('input[name=message]');
+
+    socket.emit('message', {text: $message.val()});
+
+    $message.val("");
+  });
+});
