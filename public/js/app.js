42c3a7bcf8e0a1857738ac8befaab1f1ac771011
Duong Pham <bovame94@gmail.com>
2017-08-15 22:34:32 +0700

    showing message

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,24 @@
-var socket = io();
-
-socket.on('connect', function() {
-  console.log('Connected socket.io successful');
-});
-
-socket.on('message', function(message){
-  console.log('New message:' + message.text);
-});
-
 $(document).ready(function(){
-  $("#message-form").on("submit", function(event){
+  var socket = io();
+
+  socket.on('connect', function() {
+    console.log('Connected socket.io successful');
+  });
+
+  socket.on('message', function(message){
+    console.log('New message:' + message.text);
+
+    $('#messages').append('<p>' + message.text + '</p>');
+  });
+
+
+  $('#message-form').on('submit', function(event){
     event.preventDefault();
 
     var $message = $(this).find('input[name=message]');
 
     socket.emit('message', {text: $message.val()});
 
-    $message.val("");
+    $message.val('');
   });
 });
